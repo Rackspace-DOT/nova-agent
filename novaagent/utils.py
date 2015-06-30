afb4889a1dfc92a59275d571ff8dcc2474e1bf7b
Daniel Wallace <danielwallace@gtmanfred.com>
2015-06-30 09:41:16 -0500

    validate uuid

--- novaagent/utils.py
+++ novaagent/utils.py
@@ -10,6 +10,18 @@ except ImportError as exc:
     HAS_NETIFACES = False
 
 
+def valid_uuid(uuid):
+    '''
+    validate that the 'uuid' is a uuid4
+    '''
+    regex = re.compile('^[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}\Z', re.I)
+    match = regex.match(str(uuid))
+
+    if match:
+        return uuid
+    return False
+
+
 def get_interface(mac):
     p = Popen(['xenstore-read', 'vm-data/networking/{0}'.format(mac)], stdout=PIPE, stderr=PIPE, shell=True)
     out, err = p.communicate()
@@ -63,7 +75,7 @@ def list_xen_events():
     p = Popen(['xenstore-ls', 'data/host'], stdout=PIPE, shell=True)
     out, err = p.communicate()
     out = out.decode('utf-8').split('\n')
-    out = [x.split(' = ')[0] for x in out if x]
+    out = [x.split(' = ')[0] for x in out if value_uuid(x)]
     return out
 
 
