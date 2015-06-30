f34653181b2ae3ca83241c0d38d7a330fcf66f80
Daniel Wallace <danielwallace@gtmanfred.com>
2015-06-30 17:35:25 +0000

    find the bug

--- novaagent/libs/__init__.py
+++ novaagent/libs/__init__.py
@@ -4,18 +4,21 @@ from novaagent.common.password import PasswordCommands
 
 class DefaultOS(object):
     def keyinit(self, name, value):
-        p = PasswordCommands()
-        ret = p.keyinit_cmd(value)
+        if not hasattr(self, 'p'):
+            self.p = PasswordCommands()
+        ret = self.p.keyinit_cmd(value)
         return ret
 
     def password(self, name, value):
-        p = PasswordCommands()
-        ret = p.password_cmd(value)
+        if not hasattr(self, 'p'):
+            self.p = PasswordCommands()
+        ret = self.p.password_cmd(value)
         return ret
 
     def injectfile(self, name, value):
-        f = FileInject()
-        ret = f.fileinject_cmd(value)
+        if not hasattr(self, 'f'):
+            self.f = FileInject()
+        ret = self.f.fileinject_cmd(value)
         return ret
 
 
