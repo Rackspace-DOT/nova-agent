58a766f817ed7e253fd4fc4fe5d5399a908e63a9
Daniel Wallace <danielwallace@gtmanfred.com>
2015-06-30 08:19:53 -0500

    add basic setup.py

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
 from novaagent import novaagent
-from novaagent.libs import archlinux
 
 if __name__ == '__main__':
     novaagent.main()
-    #novaagent._main()
---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import setuptools
+
+import novaagent
+
+setuptools.setup(
+    name='novaagent',
+    version=novaagent.__version__,
+    description=('Rackspace utility for reading xenstore and writing'
+                 'out information on bootup')
+    author='Daniel Wallace',
+    author_email='daniel.wallace@rackspace.com',
+    entry_points={
+        'console_scripts': [
+            'nova-agent=novaagent.novaagent:main'
+        ]
+    },
+    packages=['novaagent'],
+    install_requires=[
+        'pycrypto',
+        'netifaces'
+    ],
+    zip_safe=False
+)
