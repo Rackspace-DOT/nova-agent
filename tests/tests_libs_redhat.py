9fed59450387ec5b6512c487621f55a5c1bd2f83
davi4261 <david.kludt@rackspace.com>
2017-07-06 14:10:10 -0500

    Add in redhat test file

---
+++ tests/tests_libs_redhat.py
@@ -0,0 +1,26 @@
+
+from novaagent.libs import redhat
+from unittest import TestCase
+
+
+import mock
+
+
+class TestHelpers(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_redhat_kmsactivate(self):
+        temp = redhat.ServerOS()
+        with mock.patch('novaagent.common.kms.kms_activate') as kms:
+            kms.return_value = ("0", "")
+            message = temp.kmsactivate('name', 'value')
+
+        self.assertEqual(
+            ("0", ""),
+            message,
+            'Return value was not what was expected'
+        )
