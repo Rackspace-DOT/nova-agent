52660a5bd16f026fddd5115ffff928706b685090
davi4261 <david.kludt@rackspace.com>
2017-07-31 08:58:33 -0500

    Update setup/py and fix description on control debian file

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
 
 import setuptools
-
 import novaagent
+
 
 setuptools.setup(
     name='novaagent',
     version=novaagent.__version__,
-    description=('Rackspace utility for reading xenstore and writing'
-                 'out information on bootup'),
-    author='Daniel Wallace',
-    author_email='daniel.wallace@rackspace.com',
+    description=(
+        'Rackspace utility for reading xenstore and configuring'
+        ' guest instance on Xen server'
+    ),
+    author='David Kludt',
+    author_email='david.kludt@rackspace.com',
+    install_requires=['pyxs', 'pycrypto'],
+    extra_require={
+        'dev': ['netifaces'],
+        'test': ['mock', 'coverage', 'flake8', 'nose']
+    },
     entry_points={
         'console_scripts': [
             'nova-agent=novaagent.novaagent:main'
