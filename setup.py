99fbc12b4cf0757ca44c6c6d5e8a231f3f031f0d
Jourdan Clark <jourdan_clark@hotmail.com>
2019-01-30 18:26:09 -0700

    Add install for Alpine init script

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@
 import setuptools
 import novaagent
 import sys
-
+import os
+import stat
+from shutil import copyfile
 
 requirements = ['netifaces', 'pyxs', 'pycrypto', 'PyYaml', 'distro']
 if sys.version_info[:2] < (2, 7):
@@ -14,8 +16,33 @@ test_requirements = ['mock', 'nose']
 if sys.version_info[:2] < (2, 7):
     test_requirements.extend(['flake8 < 3', 'unittest2'])
 else:
+
     test_requirements.append('flake8')
 
+# https://stackoverflow.com/a/30463972
+def make_executable(path):
+    mode = os.stat(path).st_mode
+    mode |= (mode & 0o444) >> 2    # copy R bits to X
+    os.chmod(path, mode)
+
+# https://stackoverflow.com/a/36902139
+class PostInstallCommand(install):
+  def run(self):
+    install.run(self)
+    file_from = None
+    file_to = None
+    is_executable = True
+
+    dirname = os.path.dirname(__file__)
+
+    if os.path.exists('/etc/alpine-release'):
+      file_from = os.path.join(dirname, 'etc/nova-agent.alpine')
+      file_to = '/etc/init.d/nova-agent'
+
+    if file_from != None and file_to != None:
+      copyfile(file_from, file_to)
+      if is_executable:
+        make_executable(file_to)
 
 setuptools.setup(
     name='novaagent',
