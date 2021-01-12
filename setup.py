22db7214ab49037599ddd7d52e42dae6734aa2fc
Brian Metzler <brian.metzler@rackspace.com>
2021-01-12 11:35:36 -0600

    Add pyz build support

--- setup.py
+++ setup.py
@@ -3,7 +3,22 @@
 import setuptools
 import novaagent
 import sys
+import os
+from shutil import copytree
+from shutil import rmtree
 
+from zipapp import create_archive
+
+from pathlib import Path
+from setuptools import Command
+
+
+PYZ_RUNPY = """#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from runpy import run_module
+print("Running Python module: {0}.")
+run_module("{0}")
+""".format('novaagent')
 
 requirements = ['netifaces', 'pyxs', 'cryptography', 'PyYaml', 'distro']
 if sys.version_info[:2] < (2, 7):
@@ -15,6 +30,63 @@ if sys.version_info[:2] < (2, 7):
     test_requirements.extend(['flake8 < 3', 'unittest2'])
 else:
     test_requirements.append('flake8')
+
+
+class BuildPyz(Command):
+    """https://docs.python.org/3/library/zipapp.html#zipapp.create_archive ."""
+    description, user_options = "Creates a zipapp.", []
+
+    #
+    def initialize_options(self):
+        pass  # Dont needed, but required.
+
+    #
+    def finalize_options(self):
+        pass  # Dont needed, but required.
+
+    def run(self):
+        cur_dir = os.path.dirname(os.path.realpath(__file__))
+        dist_pyz = cur_dir + "/dist/pyz"
+        src_pyz = cur_dir + "/build/pyz"
+        try:
+            rmtree("{}/novaagent".format(dist_pyz))
+        except Exception:
+            pass
+        try:
+            rmtree("{}/novaagent".format(src_pyz))
+        except Exception:
+            pass
+        copytree("{}/novaagent".format(cur_dir),
+                 os.path.join(src_pyz, "novaagent"))
+        try:
+            os.remove("{}/novaagent/nova-agent".format(dist_pyz))
+        except Exception:
+            pass
+        fyle = Path(src_pyz).parent / '__main__.py'
+        fyle.write_text(PYZ_RUNPY)
+
+        Path("{}/novaagent".format(src_pyz)).mkdir(parents=True, exist_ok=True)
+        target_dir = "usr/"
+        Path(target_dir).mkdir(parents=True, exist_ok=True)
+        pyz_requirements = requirements
+
+        # We can't bundle cryptography in with pyz due to conflicts on
+        #  different distros
+        if 'cryptography' in pyz_requirements:
+            pyz_requirements.remove('cryptography')
+
+        cmd = "python3 -m pip install {} --target {}".format(
+            " ".join(pyz_requirements), src_pyz)
+        print("Running: {}".format(cmd))
+        os.system(cmd)
+        target_file = "{}nova-agent.pyz".format(target_dir)
+
+        create_archive(src_pyz,
+                       target=target_file,
+                       interpreter="/usr/bin/env python3",
+                       main="novaagent.novaagent:main")
+
+        print("Created: {}".format(target_file))
 
 
 setuptools.setup(
@@ -41,5 +113,8 @@ setuptools.setup(
         'novaagent.common',
         'novaagent.xenstore'
     ],
-    zip_safe=False
+    zip_safe=False,
+    cmdclass={
+        "pyz": BuildPyz
+    }
 )
